b7f4d2a2fc5c14ed095f58472c3dd5e446443fc4
raulcontreraso-bit <raulcontreraso@gmail.com>
2026-08-02 10:25:15 +0200

    Update project files and fix Sphinx extension config

--- content/conf.py
+++ content/conf.py
@@ -1,11 +1,12 @@
 import datetime
 
+# Project details
 project = 'Web Courses Vault'
 copyright = f'{datetime.date.today().year}, Raul Contreras'
 author = 'Raul Contreras'
 
 # Core Extensions
-# Note: sphinx_lesson automatically includes myst_parser internally
+# Note: sphinx_lesson automatically manages myst_parser internally
 extensions = [
     'sphinx_lesson',
     'sphinx_rtd_theme_ext_color_contrast',
@@ -13,13 +14,23 @@ extensions = [
     'sphinx_thebe',
 ]
 
+# Theme & GitHub Pages URL Configuration
 html_theme = 'sphinx_rtd_theme'
 
-# Configure live execution via MyBinder
+html_baseurl = 'https://raulcontreraso-bit.github.io/web_courses_vault_sp/'
+html_context = {
+    'display_github': True,
+    'github_user': 'raulcontreraso-bit',
+    'github_repo': 'web_courses_vault_sp',
+    'github_version': 'main/content/',
+}
+
+# Configure live code execution via MyBinder
 thebe_config = {
    "selector": "div.highlight",
    "repository_url": "https://github.com/raulcontreraso-bit/web_courses_vault_sp",
    "repository_branch": "main",
 }
 
+# Files and directories to ignore during build
 exclude_patterns = ['_build', 'venv', 'Thumbs.db', '.DS_Store']
